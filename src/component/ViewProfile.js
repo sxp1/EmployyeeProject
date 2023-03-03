7238697cd2889b1180a96230c58d77aa6fc3604c
63160253 <63160253@go.buu.ac.th>
2023-03-04 01:24:33 +0700

    Update ViewProfile

--- src/component/ViewProfile.js
+++ src/component/ViewProfile.js
@@ -36,193 +36,106 @@ import FormHelperText from "@mui/material/FormHelperText";
 import Input from "@mui/material/Input";
 import User from "./user";
 import TextField from "@mui/material/TextField";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 
 export default function Usercreate() {
-  // const {
-  //   register,
-  //   handleSubmit,
-  //   watch,
-  //   formState: { errors },
-  // } = useForm(
-   
-  // );
   const onSubmit = (data) => {
     console.log(data);
   };
   const id = useParams();
-  useEffect(()=>{
+  useEffect(() => {
     var requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
+      method: "GET",
+      redirect: "follow",
     };
     console.log(id.id);
     fetch(`http://192.168.56.117:3000/getdataupdate/${id.id}`, requestOptions)
-      .then(response => response.json())
-      .then(result => {
-      
-       
-       setlastname(result[0].lastname);
-      setemployeeid(result[0].employeeid);
-       setdepartment(result[0].department);
+      .then((response) => response.json())
+      .then((result) => {
+        setlastname(result[0].lastname);
+        setemployeeid(result[0].employeeid);
+        setdepartment(result[0].department);
         setage(result[0].age);
-      setphonnumber(result[0].phonnumber);
-       setposition(result[0].position);
+        setphonnumber(result[0].phonnumber);
+        setposition(result[0].position);
         setsalary(result[0].salary);
         setfname(result[0].firstname);
-       
       })
-      .catch(error => console.log('error', error));
-  }, [id])
-  
+      .catch((error) => console.log("error", error));
+  }, [id]);
+
   const theme = {
     spacing: [0, 2, 3, 5, 8],
   };
- 
-  const [fname,setfname] = useState('');
-  const [lastname,setlastname] = useState('');
-  const [employeeid,setemployeeid] = useState('');
-  const [department,setdepartment] = useState('');
-  const [age,setage] = useState('');
-  const [phonnumber,setphonnumber] = useState('');
-  const [position,setposition] = useState('');
-  const [salary,setsalary] = useState('');
+
+  const [fname, setfname] = useState("");
+  const [lastname, setlastname] = useState("");
+  const [employeeid, setemployeeid] = useState("");
+  const [department, setdepartment] = useState("");
+  const [age, setage] = useState("");
+  const [phonnumber, setphonnumber] = useState("");
+  const [position, setposition] = useState("");
+  const [salary, setsalary] = useState("");
 
   return (
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth="xl" sx={{ p: 5 }}>
-      <center>
-        <Paper sx={{ p: 10,width: "1100px" }} >
-          <center>
-           
-              <h2> ข้อมูลส่วนตัว </h2>
-              <form
-             
+        <center>
+          <Paper sx={{ p: 5, width: "1100px" }}>
+            <center>
+              <h1> View Profile </h1>
+              <h3> ข้อมูลส่วนตัว </h3>
+              <div
+                class="container"
+                id="outlined-number"
+                sx={{ width: "300px" }}
               >
-                <div>
-                <TextField
-                    id="outlined-basic"
-                    label="First Name"
-                    variant="outlined"
-                    required
-                    sx={{
-                      mb: 1,
-                      fontSize: "var(--joy-fontSize-sm)",
-                      width: "300px",
-                    }}
-                    value={fname}
-                    onChange={(e)=> setfname(e.target.value)}
-                  />
-                  <TextField
-                    id="outlined-basic"
-                    label="Last Name"
-                    variant="outlined"
-                    required
-                    sx={{ mb: 1, fontSize: "var(--joy-fontSize-sm)",ml: 3, width: "300px" }}
-                    value={lastname}
-                    onChange={(e)=> setlastname(e.target.value)}
-                  />
+                  <div class="col">First Name : --------------</div>
+                  <div class="col">Last Name : --------------</div>
+                  <div class="col">Age : --------------</div>
+                  <div class="col">ID Employee : --------------</div>
+                  <div class="col">Birthday : --------------</div>
+                  <div class="col">Education : --------------</div>
+                  <div class="col">Phone Number : --------------</div>
+                  <div class="col">Email : --------------</div>
+                  <div class="col">Salary : --------------</div>
+              </div>
+              <div>
+                <h3> ตำแหน่งหน้าที่ </h3>
+                <div
+                class="container"
+                id="outlined-number"
+                sx={{ width: "100px" }}
+               >
+                  <div class="col">Department : --------------</div>
+                  <div class="col">Position : --------------</div>
                 </div>
-                <div>
-                <TextField
-                    id="outlined-number"
-                    label="Age"
-                    type="number"
-                    InputLabelProps={{ shrink: true }}
-                    inputProps={{ min: 1 }}
-                    margin="normal"
-                    required
-                    sx={{ width: "300px" }}
-                    value={age}
-                    onChange={(e)=> setage(e.target.value)}
-                  />
-                  <TextField
-                    id="outlined-basic"
-                    label="ID Employee"
-                    variant="outlined"
-                    required
-                    sx={{ mb: 1, fontSize: "var(--joy-fontSize-sm)",ml: 3, width: "300px" }}
-                    value={employeeid}
-                    onChange={(e)=> setemployeeid(e.target.value)}
-                    margin="normal"
-                  />
+              </div>
+              <div>
+                <h3> ช่องทางการติดต่อ / เงินเดือน </h3>
+                <div
+                class="container"
+                id="outlined-number"
+                sx={{ width: "300px" }}
+               >
+                  <div class="col">Address : --------------</div>
+                  <div class="col">Phone Number : --------------</div>
+                  <div class="col">Email : --------------</div>
+                  <div class="col">Salary : --------------</div>
                 </div>
-                
-                <div>
-                <h2> ตำแหน่งหน้าที่ </h2>
-                <TextField
-                    id="outlined-basic"
-                    label="Department"
-                    variant="outlined"
-                    required
-                    sx={{
-                      mb: 1,
-                      fontSize: "var(--joy-fontSize-sm)",
-                      width: "300px",
-                    }}
-                    onChange={(e)=> setdepartment(e.target.value)}
-                    value={department}
-                    margin="normal"
-                  />
-                  <TextField
-                    id="outlined-basic"
-                    label="Posittion"
-                    variant="outlined"
-                    required
-                    sx={{ mb: 1, fontSize: "var(--joy-fontSize-sm)",ml: 3, width: "300px" }}
-                    onChange={(e)=> setposition(e.target.value)}
-                    value={position}
-                    margin="normal"
-                  />
-                 
-                 
-                </div>
-                <div>
-                <h2> ช่องทางการติดต่อ / เงินเดือน </h2>
-                <TextField
-                    id="outlined-basic"
-                    label="Phonnumber"
-                    variant="outlined"
-                    required
-                    sx={{
-                      mb: 1,
-                      fontSize: "var(--joy-fontSize-sm)",
-                      width: "300px",
-                    }}
-                    onChange={(e)=> setphonnumber(e.target.value)}
-                    value={phonnumber}
-                    margin="normal"
-                  />
-                  <TextField
-                    id="outlined-basic"
-                    label="Salary"
-                    variant="outlined"
-                    required
-                    sx={{ mb: 1, fontSize: "var(--joy-fontSize-sm)",ml: 3, width: "300px" }}
-                    onChange={(e)=> setsalary(e.target.value)}
-                    value={salary}
-                    margin="normal"
-                  />
-                 
-                 
-                </div>
-              
-                
-                <Button
-                  variant="contained"
-                  href="/"
-                  sx={{ mt: 3  }}
-                  color="success"
-                  
-                >
-                  Black
-                </Button>
-              
-              </form>
-          
-          </center>
-        </Paper>
+              </div>
+
+              <Button
+                variant="contained"
+                href="/"
+                sx={{ mt: 3 }}
+                color="success"
+              >
+                Back
+              </Button>
+            </center>
+          </Paper>
         </center>
       </Container>
     </React.Fragment>
